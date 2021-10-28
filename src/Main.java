413993436ee6376d0826dc4ee0cf9e15ee4f5eb2
Nade Korshunova <naanko2021@gmail.com>
2021-10-28 15:07:01 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1010;
+        int costOfaMile = 20;
+        int mile = ticketPrice / costOfaMile;
+        System.out.println( mile + " мили");
+
+    }
+}
